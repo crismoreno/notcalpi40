074c0f19053f191dcf35138e2bacbbcc9a818d03
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-03-29 01:32:46 +0100

    testing deploy

--- server/index.js
+++ server/index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
 const app = express();
 
-//Middleware
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -14,8 +13,8 @@ const contents = require('./routes/api/contents');
 app.use('/api', contents)
 
 //Handdle Production
-if(process.env.NODE_ENV === 'production') {
-	app.use(express.static(__dirname + '/public/'))
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(__dirname + '/public/'));
 
 	//Handle SPA
 	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
@@ -23,4 +22,4 @@ if(process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
