6eb65c7e6c2c2b5a97e41105b652ad2e6b22450e
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-03-29 01:01:11 +0100

    ready for deploy

--- server/index.js
+++ server/index.js
@@ -13,6 +13,13 @@ const contents = require('./routes/api/contents');
 
 app.use('/api', contents)
 
+//Handdle Production
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static(__dirname + '/public/'))
+
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
