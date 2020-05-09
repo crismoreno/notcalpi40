8b2af424d7e3ddaeca7e1ed23f5635bc0f67c934
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-05-09 10:43:20 +0200

    got sourcemaps working fine in this commit

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,35 +2,36 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 // const browserSync = require("browser-sync").create();
-const minify = require('gulp-minify');
-const notify = require( 'gulp-notify' );
+// const minify = require("gulp-minify");
+const notify = require("gulp-notify");
+const sourcemaps = require("gulp-sourcemaps");
 
-
-
-//compile scss into css
 function cssfy() {
-    //1. where is my scss file
-    return (
-      gulp
-        .src("./src/assets/styles/importer.scss")
-        //2. pass that file through the sass compiler
-        .pipe(sass({outputStyle: 'compressed'}))
-        //3. where do I save the compiled CSS?
-        .pipe(gulp.dest("./src/assets/styles/"))
-        //4. stream changes to all browser
-        // .pipe(browserSync.stream())
-        .pipe( notify({ message: '\n\n✅  ===> STYLE — completed!\n', onLast: true }) )
-    );
+  // gulp.task("cssfy", function() {
+  return (
+    gulp
+      //   //1. where is my scss file
+      .src("./src/assets/styles/importer.scss")
+      .pipe(sourcemaps.init())
+      //       //2. pass that file through the sass compiler
+      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+      .pipe(sourcemaps.write())
+      //       //3. where do I save the compiled CSS?
+      .pipe(gulp.dest("./src/assets/styles/"))
+      .pipe(
+        notify({ message: "\n\n✅  ===> STYLE — completed!\n", onLast: true })
+      )
+  );
+  // });
 }
-
 
 function watch() {
-    // browserSync.init({
-    //     server: { baseDir: "./" }
-    // });
-    gulp.watch("./src/assets/styles/**/*.scss", cssfy);
-    gulp.watch("./src/assets/styles/**/*.scss");
+  // browserSync.init({
+  //     server: { baseDir: "./" }
+  // });
+  gulp.watch("./src/assets/styles/**/*.scss", cssfy);
+  gulp.watch("./src/assets/styles/**/*.scss");
 }
-  
+
 exports.cssfy = cssfy;
 exports.watch = watch;
