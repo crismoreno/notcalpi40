57763a613c0c96d5482d3e4ffb1dd41d4a18f8d7
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-05-10 12:23:20 +0200

    some methods on the back

--- server/controllers/projects.controller.js
+++ server/controllers/projects.controller.js
@@ -2,47 +2,48 @@ const db = require("../models");
 const Projects = db.projects;
 const Op = db.Sequelize.Op;
 
-
 // Retrieve all Projects from the database.
 exports.findAll = (req, res) => {
-	const title = req.query.title;
-	var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-  
-	Projects.findAll({ where: condition })
-	  .then(data => {
-		res.send(data);
-	  })
-	  .catch(err => {
-		res.status(500).send({
-		  message:
-			err.message || "Some error occurred while retrieving tutorials."
-		});
-	  });
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+
+  Projects.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving projects.",
+      });
+    });
 };
 
-// // Find a single Project with an id
-// exports.findOne = (req, res) => {
-// 	Projects.findByPk(id)
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message: "Error retrieving Tutorial with id=" + id
-//       });
-//     });
-// };
+// Find a single Project with an id
+exports.findById = (req, res) => {
+  const idToFetch = req.params.id;
+  Projects.findAll({ where: { id: idToFetch } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving Project with id=" + id,
+      });
+    });
+};
 
-// // Find all features Projects
-// exports.findAllFeatured = (req, res) => {
-// 	Projects.findAll({ where: { is_featured: true } })
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || "Some error occurred while retrieving tutorials."
-//       });
-//     });
-// };
+// Find all featured Projects
+exports.findAllFeatured = (req, res) => {
+  Projects.findAll({ where: { is_featured: true } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while retrieving featured projects.",
+      });
+    });
+};
--- server/routes/contents.js
+++ server/routes/contents.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const projectsController = require("../controllers/projects.controller.js");
 
 const router = express.Router();
 
 //Retreiving all projects
 router.get("/", projectsController.findAll);
+router.get("/featured", projectsController.findAllFeatured);
+router.get("/project/:id", projectsController.findById);
 
 module.exports = router;
