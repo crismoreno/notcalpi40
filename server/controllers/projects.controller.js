80a2e3898e92dc3cf8de5b00d8a82d232e914ed8
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-05-10 19:50:39 +0200

    got some endpoints in this commit

--- server/controllers/projects.controller.js
+++ server/controllers/projects.controller.js
@@ -1,13 +1,15 @@
 const db = require("../models");
 const Projects = db.projects;
 const Op = db.Sequelize.Op;
+const Sequelize = db.sequelize;
+const { QueryTypes } = require("sequelize");
 
 // Retrieve all Projects from the database.
-exports.findAll = (req, res) => {
+exports.getAll = (req, res) => {
   const title = req.query.title;
   var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
 
-  Projects.findAll({ where: condition })
+  Projects.findAll({ where: { show: true } })
     .then((data) => {
       res.send(data);
     })
@@ -20,7 +22,7 @@ exports.findAll = (req, res) => {
 };
 
 // Find a single Project with an id
-exports.findById = (req, res) => {
+exports.getById = (req, res) => {
   const idToFetch = req.params.id;
   Projects.findAll({ where: { id: idToFetch } })
     .then((data) => {
@@ -34,10 +36,130 @@ exports.findById = (req, res) => {
 };
 
 // Find all featured Projects
-exports.findAllFeatured = (req, res) => {
+exports.getAllFeatured = (req, res) => {
   Projects.findAll({ where: { is_featured: true } })
     .then((data) => {
       res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while retrieving featured projects.",
+      });
+    });
+};
+
+// Find all projects by tags
+exports.getByTag = (req, res) => {
+  let tagsToFetch = req.query.tags;
+  tagsToFetch = tagsToFetch.split(",");
+
+  // Sequelize.query(`SELECT * FROM projects WHERE tags IN('%${tagsToFetch}%')`, {
+  //   type: QueryTypes.SELECT,
+  // })
+  //   .then((data) => {
+  //     res.send(data);
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).send({
+  //       message:
+  //         err.message ||
+  //         "Some error occurred while retrieving featured projects.",
+  //     });
+  //   });
+
+  Projects.findAll({})
+    .then((data) => {
+      const elementsToReturn = [];
+      let checker = (arr, target) => target.every((v) => arr.includes(v));
+
+      data.forEach((project) => {
+        const check = checker(project.tags, tagsToFetch);
+        if (check) {
+          elementsToReturn.push(project);
+        }
+      });
+
+      res.send(elementsToReturn);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while retrieving featured projects.",
+      });
+    });
+};
+
+// Find all projects by filterplace
+exports.getByFilterPlace = (req, res) => {
+  let placeToFetch = req.query.places;
+
+  Projects.findAll({ where: { placefilter: placeToFetch } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while retrieving featured projects.",
+      });
+    });
+};
+
+// exports.getAllPlaces = async (req, res) => {
+//   const places = await Sequelize.query(
+//     "SELECT DISTINCT `placefilter` FROM `projects`",
+//     { type: QueryTypes.SELECT }
+//   );
+//   const array = places.map((place) => {
+//     return place.placefilter;
+//   });
+//   res.send(array);
+// };
+
+// exports.getAllTags = async (req, res) => {
+//   Sequelize.query("SELECT DISTINCT `tags` FROM `projects`", {
+//     type: QueryTypes.SELECT,
+//   })
+//     .then((data) => {
+//       let array = data.map((tag) => {
+//         return tag.tags;
+//       });
+//       array.filter((item, index) => {
+//         return array.indexOf(item) === index;
+//       });
+//       res.send(array);
+//     })
+//     .catch((err) => {
+//       res.status(500).send({
+//         message:
+//           err.message ||
+//           "Some error occurred while retrieving featured projects.",
+//       });
+//     });
+// };
+
+// Find all projects by tags
+exports.getByCodingLang = (req, res) => {
+  let tagsToFetch = req.query.code;
+  tagsToFetch = tagsToFetch.split(",");
+
+  Projects.findAll({})
+    .then((data) => {
+      const elementsToReturn = [];
+      let checker = (arr, target) => target.every((v) => arr.includes(v));
+
+      data.forEach((project) => {
+        const check = checker(project.coding_lang, tagsToFetch);
+        if (check) {
+          elementsToReturn.push(project);
+        }
+      });
+
+      res.send(elementsToReturn);
     })
     .catch((err) => {
       res.status(500).send({
--- server/routes/contents.js
+++ server/routes/contents.js
@@ -4,8 +4,14 @@ const projectsController = require("../controllers/projects.controller.js");
 const router = express.Router();
 
 //Retreiving all projects
-router.get("/", projectsController.findAll);
-router.get("/featured", projectsController.findAllFeatured);
-router.get("/project/:id", projectsController.findById);
-
+router.get("/", projectsController.getAll);
+router.get("/featured", projectsController.getAllFeatured);
+router.get("/project/:id", projectsController.getById);
+//http://localhost:5000/api/tags?tags=code,html
+router.get("/tags", projectsController.getByTag);
+// router.get("/tags/list", projectsController.getAllTags);
+//http://localhost:5000/api/filterplace?places=esdi
+router.get("/filterplace", projectsController.getByFilterPlace);
+// router.get("/filterplace/list", projectsController.getAllPlaces);
+router.get("/codinglang", projectsController.getByCodingLang);
 module.exports = router;
