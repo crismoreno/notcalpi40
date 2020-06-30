805946cc07384e02b4203e6f84216eb47f069ebf
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-06-30 23:03:33 +0200

    new sharing link in this commit

--- client/src/views/AboutMe.vue
+++ client/src/views/AboutMe.vue
@@ -12,7 +12,7 @@
         <div class="d-flex flex-column flex-xl-row flex-lg-row flex-md-row flex-sm-row">
           <a
             role="button"
-            href="https://drive.google.com/file/d/19qrsXbRcoTB1MIR2w6CEmZg-XTNtgpuS/view?usp=sharing"
+            href="https://drive.google.com/file/d/1CsV5F2uyxXGci00PZgbZY1QQ8yUFSmE2/view?usp=sharing"
           >DOWNLOAD MY CV</a>
           <a
             role="button"
